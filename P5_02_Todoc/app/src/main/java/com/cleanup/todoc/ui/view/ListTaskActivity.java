1edf23d2a505e4d3bf06bff2470aacf848de90af
DsMikael <mickael.silva@live.com>
2021-05-28 15:51:32 +0200

    mainActivity error repair

--- P5_02_Todoc/app/src/main/java/com/cleanup/todoc/ui/view/ListTaskActivity.java
+++ P5_02_Todoc/app/src/main/java/com/cleanup/todoc/ui/view/ListTaskActivity.java
@@ -1,13 +1,14 @@
 package com.cleanup.todoc.ui.view;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -32,7 +33,7 @@ import java.util.Date;
  *
  * @author Gaëtan HERFRAY
  */
-public class MainActivity extends AppCompatActivity implements TasksAdapter.DeleteTaskListener {
+public class ListTaskActivity extends AppCompatActivity implements TasksAdapter.DeleteTaskListener {
     /**
      * List of all projects available in the application
      */
@@ -83,7 +84,8 @@ public class MainActivity extends AppCompatActivity implements TasksAdapter.Dele
      */
     private TextView lblNoTasks;
 
-    public MainActivity() {
+
+    public ListTaskActivity() {
         listTasks = null;
         lblNoTasks = null;
     }
---
+++ P5_02_Todoc/app/src/main/java/com/cleanup/todoc/ui/viewModel/MainActivityViewModel.java
@@ -0,0 +1,14 @@
+package com.cleanup.todoc.ui.viewModel;
+
+import android.app.Application;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.annotation.NonNull;
+
+import org.jetbrains.annotations.NotNull;
+
+public class MainActivityViewModel extends AndroidViewModel {
+
+    public MainActivityViewModel(@NonNull @NotNull Application application) {
+        super(application);
+    }
+}
